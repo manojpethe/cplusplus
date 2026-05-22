f567bf3070dcd222e10803ff0443892d1663e568
Manoj Pethe <manoj.pethe@siemens.com>
2026-05-22 16:11:09 +0530

    updates

--- sortFunction.cpp
+++ sortFunction.cpp
@@ -7,16 +7,29 @@ int main(){
     int newArray[5];
     int largestIntIndex = 0;
     int largestInt = 0;
-    for(int ni = 0 ; ni < size(myArray) ; ni++){
-        if(myArray[ni] == -1){
-            continue;
+    for(int i = 0; i < size(myArray) ; i++){
+        // if(myArray[i] == -1){
+        //     continue;
+        // }
+        for(int ni = 0 ; ni < size(myArray) ; ni++){
+            if(myArray[ni] == -1){
+                continue;
+            }
+            if(myArray[ni] > myArray[ni+1] ){
+                cout << myArray[ni] << " > than " << myArray[ni+1] << endl;
+                largestInt = myArray[ni];
+                largestIntIndex = ni;
+            }
+            cout << "Iteration:" << ni << " " << myArray[ni] << " " <<  largestInt << " " << largestIntIndex  << endl;
         }
-        if(myArray[ni] > myArray[ni+1] && myArray[ni] > largestInt ){
-            cout << myArray[ni] << " > than " << myArray[ni+1] << endl;
-            largestInt = myArray[ni];
-            largestIntIndex = ni;
-        }
-        cout << "Iteration:" << ni << " " << myArray[ni] << " " <<  largestInt << " " << largestIntIndex  << endl;
+        // add the largest integer to newArray
+        newArray[i] = largestInt;
+        // update the value of existing array element to -1 to skip it next time.
+        myArray[largestIntIndex] = -1;
+
+    }
+    for(int i = 0; i < 5; i++){
+        cout << newArray[i] << endl;
     }
 
 }
